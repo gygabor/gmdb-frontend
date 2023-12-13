259486c236c9a4eaa910bed80c05aa97a17cb086
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-13 18:58:55 +0100

    feat(Movies): implement Paginator

--- src/pages/Movies/Movies.tsx
+++ src/pages/Movies/Movies.tsx
@@ -1,22 +1,31 @@
 import { FC } from 'react'
-import { Container, Box } from '@mui/material'
-import SearchInput from '@components/SearchInput'
+import { Container } from '@mui/material'
 import MovieList from '@components/MovieList'
 import { mockedList } from './mockedList'
+import Search from '@src/components/Search'
+import Paginatior from '@src/components/Paginator'
 
 const Movies: FC = () => {
   const handleChange = (value: string) => {
     console.log(value)
   }
 
+  const handleSearch = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.target)
+  }
+
+  const handlePaginatorChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number,
+  ) => {
+    console.log(page)
+  }
+
   return (
     <Container>
-      <Box>
-        <SearchInput handleChange={handleChange} />
-      </Box>
-      <Container>
-        <MovieList movies={mockedList} />
-      </Container>
+      <Search handleChange={handleChange} onClick={handleSearch} />
+      <MovieList movies={mockedList} />
+      <Paginatior onChange={handlePaginatorChange} />
     </Container>
   )
 }
