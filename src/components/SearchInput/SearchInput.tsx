a12d7fc3ab93e7b5a91c4c61471aba12bb34a73d
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-13 13:33:46 +0100

    refactor(SearchInput): change event import

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, ChangeEvent } from 'react'
+import { FC, useState } from 'react'
 import { TextField, InputAdornment } from '@mui/material'
 import ClearIcon from '@mui/icons-material/Clear'
 import SearchIcon from '@mui/icons-material/Search'
@@ -12,7 +12,7 @@ const SearchInput: FC<Props> = ({ handleChange }) => {
   const [isClearIcon, setisClearIcon] = useState<boolean>(false)
   const [value, setValue] = useState<string>('')
 
-  const onTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value)
     handleChange(event.target.value)
     event.target.value ? setisClearIcon(true) : setisClearIcon(false)
