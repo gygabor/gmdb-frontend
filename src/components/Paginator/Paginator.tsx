7ff853b36ae70663da9e514d273f1ab8f10292bb
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-13 18:57:51 +0100

    feat: create Paginator

---
+++ src/components/Paginator/Paginator.tsx
@@ -0,0 +1,23 @@
+import { Pagination } from '@mui/material'
+import { FC } from 'react'
+import { PaginatorBox } from './styles'
+
+interface Props {
+  onChange: (event: React.ChangeEvent<unknown>, page: number) => void
+}
+
+const Paginator: FC<Props> = ({ onChange }) => {
+  return (
+    <PaginatorBox>
+      <Pagination
+        count={10}
+        shape="rounded"
+        showFirstButton
+        showLastButton
+        onChange={onChange}
+      />
+    </PaginatorBox>
+  )
+}
+
+export default Paginator
