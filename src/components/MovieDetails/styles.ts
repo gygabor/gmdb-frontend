9ce948df2ba83a6f91180ff80da11381c484d303
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-14 17:16:48 +0100

    feat(MovieDetails): style component

---
+++ src/components/MovieDetails/styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+import { Box } from '@mui/material'
+
+export const ContentBox = styled(Box)(() => ({
+  width: '100%',
+  display: 'flex',
+  alignItems: 'baseline',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap',
+}))
