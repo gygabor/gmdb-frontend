f090069fe4dee1cc736b2c5e86c7e004af7bfa90
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-15 14:56:36 +0100

    feat(components): create indicator

---
+++ src/components/SourceIndicator/SourceIndicator.tsx
@@ -0,0 +1,23 @@
+import { CircularProgress } from '@mui/material'
+import { FC } from 'react'
+import { IndicatorAvatar, IndicatorBox, IndicatorProgress } from './styles'
+
+interface Props {
+  source: 'tmdb' | 'cache' | null
+  isLoading: boolean
+}
+
+const SourceIndicator: FC<Props> = ({ source, isLoading }) => {
+  return (
+    <IndicatorBox>
+      {source ? (
+        <IndicatorAvatar source={source}>
+          {source.charAt(0).toUpperCase()}
+        </IndicatorAvatar>
+      ) : null}
+      {isLoading && <IndicatorProgress size={46} source={source} />}
+    </IndicatorBox>
+  )
+}
+
+export default SourceIndicator
